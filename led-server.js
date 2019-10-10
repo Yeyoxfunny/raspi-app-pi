14bb074a98f8c33a7f8574e9b61420489da0b5d9
Sergio Morales <yeyox_114@hotmail.com>
2019-10-09 23:44:31 -0500

    Test script

--- led-server.js
+++ led-server.js
@@ -1,34 +1,20 @@
-var socket = require('socket.io-client')('http://192.168.0.2:3000');
-
-socket.on('connect', function(){
-    console.log('Hello friend');
-});
-
-
-var Cylon = require('cylon');
 var timeTool = require('./utilities/time-tool');
 var myShutdown;
 
-Cylon.robot({
-	connections: { raspi: { adaptor: 'raspi'} },
-	devices: {
-        led: { driver: 'led', pin: 3 }
-    },
-    work: function (my) {
-        socket.on('toggleServer',function(data){
-            my.led.toggle();
-        });
-        socket.on('shutdownServer',function(data){
-            var delay = isNaN(data.delay)?5000:data.delay;
-            delay = timeTool(delay, data.option);
-            console.log(delay);
-            myShutdown = setTimeout(function(){
-                my.led.toggle();
-                socket.emit('togglePi',{isOn: my.led.isOn(), isPong: true});
-            }, delay);
-        });
-        socket.on('cancelShutdownServer',function(){
-            clearTimeout(myShutdown);
-        });
-    }
-}).start();
+
+const socket = require('socket.io-client')('http://192.168.0.2:3000');
+
+const five = require("johnny-five");
+const Raspi = require("raspi-io").RaspiIO;
+const board = new five.Board({
+  io: new Raspi()
+});
+
+socket.on('connect', function() {
+    console.log('Hello friend');
+});
+
+board.on("ready", function() {
+    const led = new five.Led("P1-13");
+    led.blink();
+  });
